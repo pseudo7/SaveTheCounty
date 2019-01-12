c7b1161fdb51926a14180006eb39c6a725f9f43f
Pseudo <enikhil7@gmail.com>
2019-01-13 01:24:41 +0530

    Added GameManager

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,5 @@
+﻿public class Constants
+{
+    public const int LEVEL1_EXIT_TIME = 10;
+    public const int LEVEL2_EXIT_TIME = 10;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameObject ufo;
+    public GameObject suicider;
+    public GameObject motherShip;
+
+    public int ufoSpawnTime;
+    public int suiciderSpawnTime;
+    public int motherShipSpawnTime;
+
+    float countdown;
+
+    readonly WaitForEndOfFrame WAIT_FOR_ENDFRAME = new WaitForEndOfFrame();
+
+    private void Awake()
+    {
+        if (!Instance)
+            Instance = this;
+    }
+
+    void Start()
+    {
+        StartCoroutine(LevelInfoCheck());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    IEnumerator LevelInfoCheck()
+    {
+        while (gameObject.activeInHierarchy)
+        {
+            if (countdown % motherShipSpawnTime == 0)
+                SpawnMotherShip();
+            else if (countdown % suiciderSpawnTime == 0)
+                SpawnSuicider();
+            else if (countdown % ufoSpawnTime == 0)
+                SpawnUFO();
+            else countdown += Time.deltaTime;
+            yield return WAIT_FOR_ENDFRAME;
+        }
+    }
+
+    public void UpdateLevelInfo(LevelInfo levelInfo)
+    {
+        ufoSpawnTime = levelInfo.ufoSpawnTime;
+        suiciderSpawnTime = levelInfo.suiciderSpawnTime;
+        motherShipSpawnTime = levelInfo.motherShipSpawnTime;
+    }
+
+    void SpawnUFO() { }
+
+    void SpawnSuicider() { }
+
+    void SpawnMotherShip() { }
+}
