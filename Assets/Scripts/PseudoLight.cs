720a0128d66e42b9ce575032f421b67f6cdbf8d4
Pseudo <enikhil7@gmail.com>
2019-01-13 00:00:54 +0530

    Added Prefabs

--- Assets/Scripts/PseudoLight.cs
+++ Assets/Scripts/PseudoLight.cs
@@ -6,9 +6,9 @@ public class PseudoLight : MonoBehaviour
 {
     public int lightAngle = 20;
     public int lightBounds = 120;
+    public float moveSpeed = 10;
 
     Transform leftFlank, rightFlank;
-    Quaternion leftFlankRotation, rightFlankRotation;
 
     private void Start()
     {
@@ -18,8 +18,20 @@ public class PseudoLight : MonoBehaviour
 
     private void Update()
     {
+        SetupLight();
+    }
+
+    void SetupLight()
+    {
         lightAngle = Mathf.Clamp(lightAngle, 0, 180);
         leftFlank.localRotation = Quaternion.Euler(0, 0, lightAngle / 2);
         rightFlank.localRotation = Quaternion.Euler(0, 0, 180 - lightAngle / 2);
+    }
+
+    void MoveLight()
+    {
+#if UNITY_EDITOR
+        transform.Rotate(0, 0, Time.deltaTime * moveSpeed)
+#endif
     }
 }
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public int health = 10;
+    public float fireRate = 2;
+    public Vector3 rotationAngle = Vector3.up;
+    public float rotationSpeed = 100;
+
+    bool dead;
+
+    private void Update()
+    {
+        if (!dead)
+            transform.Rotate(rotationAngle * Time.deltaTime * rotationSpeed);
+    }
+
+    void BulletHit()
+    {
+        if (--health <= 0)
+            dead = true;
+    }
+}
