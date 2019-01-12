25ba3bc682b5d6a5884aca817a30e0568ea6d742
Pseudo <enikhil7@gmail.com>
2019-01-12 22:50:49 +0530

    Init Commit

---
+++ Assets/Scripts/PseudoLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PseudoLight : MonoBehaviour
+{
+    public int lightAngle = 20;
+    public int lightBounds = 120;
+
+    Transform leftFlank, rightFlank;
+    Quaternion leftFlankRotation, rightFlankRotation;
+
+    private void Start()
+    {
+        leftFlank = transform.GetChild(0);
+        rightFlank = transform.GetChild(1);
+    }
+
+    private void Update()
+    {
+        lightAngle = Mathf.Clamp(lightAngle, 0, 180);
+        leftFlank.localRotation = Quaternion.Euler(0, 0, lightAngle / 2);
+        rightFlank.localRotation = Quaternion.Euler(0, 0, 180 - lightAngle / 2);
+    }
+}
